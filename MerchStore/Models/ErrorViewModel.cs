0b5b80e7a37913c5a13d5013dc6c142ab2ec94af
23014636 <23014636@sfcollege.edu>
2025-10-17 20:19:08 -0400

    Add project files.

---
+++ MerchStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MerchStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
